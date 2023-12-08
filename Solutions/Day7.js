a8fc0b1029899146a58fec9ceb4a1692e9d7ec24
RubenRebel <rubenberniesgames@gmail.com>
2023-12-08 23:38:04 +0100

    day 7 finished

--- Solutions/Day7.js
+++ Solutions/Day7.js
@@ -1,16 +1,30 @@
 import { SplitOnNewLines } from "../Helpers/FileReader.js";
+import { GetAmountOfCharInString } from "../Helpers/StringUtils.js";
 import { Run } from "./SolutionBase.js";
 
-function PartOne() {
+function PartOne(){
+  return Solve(false); 
+}
+
+function PartTwo(){
+  return Solve(true); 
+}
+
+function Solve(partTwo) {
   const hands = SplitOnNewLines("day7.txt");
 
   while (true) {
     let swapped = false;
     for (let i = 0; i < hands.length - 1; i++) {
-      const handOne = hands[i].split(" ").shift();
-      const handTwo = hands[i + 1].split(" ").shift();
-
-      if (NeedsSwap(handOne, handTwo)) {
+      const originalHandOne = hands[i].split(" ").shift();
+      const originalHandTwo = hands[i + 1].split(" ").shift();
+      let handOne = hands[i].split(" ").shift();
+      let handTwo = hands[i + 1].split(" ").shift();
+      if(partTwo){
+        handOne = CheckForJokers(handOne);
+        handTwo = CheckForJokers(handTwo);
+      }
+      if (NeedsSwap(handOne, handTwo, originalHandOne, originalHandTwo)) {
         swapped = true;
         [hands[i], hands[i + 1]] = [hands[i + 1], hands[i]];
       }
@@ -28,7 +42,35 @@ function PartOne() {
   return result;
 }
 
-function NeedsSwap(handOne, handTwo) {
+function CheckForJokers(hand){
+  const joker = "J";
+
+  if(GetAmountOfSameInHand(hand) == 5 && hand[0] == joker){
+    return hand;
+  }
+  const mostCommonCard = maxCount(hand);
+
+  for(let i = 0; i < hand.length; i++){
+    if(hand[i] == joker){
+      hand = hand.replace(hand[i], mostCommonCard);
+    }
+  }
+  return hand;
+}
+
+function maxCount(input) {
+  var max = 0,
+     maxChar = '';
+     input.split('').forEach(function(char){
+    if(input.split(char).length > max && char != 'J') {
+        max = input.split(char).length;
+        maxChar = char;
+     }
+  });
+  return maxChar;
+}
+
+function NeedsSwap(handOne, handTwo, originalHandOne, originalHandTwo) {
   const handOneSameOfKind = GetAmountOfSameInHand(handOne);
   const handTwoSameOfKind = GetAmountOfSameInHand(handTwo);
 
@@ -41,7 +83,7 @@ function NeedsSwap(handOne, handTwo) {
   }
 
   if (handOneSameOfKind == 5) {
-    return (GetCardValue(GetFirstCharFromString(handOne)) > GetCardValue(GetFirstCharFromString(handTwo)));
+    return FirstHandHasHighestCard(originalHandOne, originalHandTwo);
   }
 
   if (IsFullHouse(handOne) && !IsFullHouse(handTwo)) {
@@ -63,7 +105,7 @@ function NeedsSwap(handOne, handTwo) {
     return false;
   }
 
-  return FirstHandHasHighestCard(handOne, handTwo);
+  return FirstHandHasHighestCard(originalHandOne, originalHandTwo);
 }
 
 function FirstHandHasHighestCard(firstHand, secondHand) {
@@ -85,7 +127,13 @@ function IsFullHouse(input) {
       uniq += input[i];
     }
   }
-  return uniq.length == 2;
+  if(uniq.length == 2){
+    if(GetAmountOfCharInString(uniq[0], input) == 4 || GetAmountOfCharInString(uniq[1], input) == 4){
+      return false;
+    }
+    return true;
+  }
+  return false;
 }
 
 function GetAmountOfSameInHand(input) {
@@ -121,7 +169,10 @@ function GetAmountOfPairs(input) {
 }
 
 function GetCardValue(input) {
-  const order = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A'];
+  // order for part 1
+  // const order = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A'];
+  // order for part 2
+  const order = ['J', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'Q', 'K', 'A'];
   return order.indexOf(input);
 }
 
@@ -129,4 +180,5 @@ function GetFirstCharFromString(input) {
   return Array.from(input)[0];
 }
 
-Run("Day 6 part two", "Camel Cards", PartOne);
+// Run("Day 6 part one", "Camel Cards", PartOne);
+Run("Day 7 part two", "Camel Cards", PartTwo);
