f8f341e1b0fc0692f331974ae195a8f248f93f41
RubenRebel <rubenberniesgames@gmail.com>
2023-12-08 20:30:11 +0100

    part two done

--- Solutions/Day8.js
+++ Solutions/Day8.js
@@ -2,7 +2,7 @@ import { GetRawInput } from "../Helpers/FileReader.js";
 import { Run } from "./SolutionBase.js";
 
 
-function PartOne(){
+function PartOne() {
   const input = GetRawInput("day8.txt");
   const steps = input.split(/\n\s*\n/).shift();
   const locations = input.split(/\n\s*\n/).pop().split(/\r?\n/);
@@ -11,22 +11,20 @@ function PartOne(){
   const tree = CreateNodeTree(locations);
 
   let currentPosition = "AAA";
-  let directionOptions = tree[currentPosition];
 
   let stepsTaken = 0;
-  while(currentPosition != destination){
-    for(let i = 0; i < steps.length; i++){
-      if(steps[i] == "L"){
-        currentPosition = directionOptions.left;
+  while (currentPosition != destination) {
+    for (let i = 0; i < steps.length; i++) {
+      if (steps[i] == "L") {
+        currentPosition = tree[currentPosition].left;
       }
 
-      if(steps[i] == "R"){
-        currentPosition = directionOptions.right;
+      if (steps[i] == "R") {
+        currentPosition = tree[currentPosition].right;
       }
 
-      directionOptions = tree[currentPosition];
       stepsTaken++;
-      if(currentPosition == destination){
+      if (currentPosition == destination) {
         return stepsTaken;
       }
     }
@@ -34,9 +32,59 @@ function PartOne(){
   return stepsTaken;
 }
 
-function CreateNodeTree(locations){
+function PartTwo() {
+  const input = GetRawInput("day8.txt");
+  const steps = input.split(/\n\s*\n/).shift();
+  const locations = input.split(/\n\s*\n/).pop().split(/\r?\n/);
+  const tree = CreateNodeTree(locations);
+
+  let currentPositions = Object.keys(tree).filter(key => key[key.length - 1] == 'A');
+  const result = [];
+  for (let y = 0; y < currentPositions.length; y++) {
+    let stepsTaken = 0;
+    let notFound = true;
+    while (notFound) {
+      for (let i = 0; i < steps.length; i++) {
+        if (steps[i] == "L") {
+          currentPositions[y] = tree[currentPositions[y]].left;
+        }
+
+        if (steps[i] == "R") {
+          currentPositions[y] = tree[currentPositions[y]].right;
+        }
+        stepsTaken++;
+
+        if (currentPositions[y][currentPositions[y].length - 1] == 'Z') {
+          result.push(stepsTaken);
+          notFound = false;
+          break;
+        }
+      }
+    }
+  }
+  return leastCommonMultiple(result);
+}
+
+function leastCommonMultiple(input) {
+  function gcd(a, b) {
+      return !b ? a : gcd(b, a % b);
+  }
+
+  function lcm(a, b) {
+      return (a * b) / gcd(a, b);   
+  }
+
+  var multiple = input[0];
+  input.forEach(function(n) {
+      multiple = lcm(multiple, n);
+  });
+
+  return multiple;
+}
+
+function CreateNodeTree(locations) {
   const map = {};
-  for(let i = 0; i < locations.length; i++){
+  for (let i = 0; i < locations.length; i++) {
     const nodeName = locations[i].split('=').shift().trim();
     const left = locations[i].split('=').pop().trim().split(",")[0].replace('(', '');
     const right = locations[i].split('=').pop().trim().split(",")[1].trim().replace(')', '');
@@ -49,3 +97,4 @@ function CreateNodeTree(locations){
 }
 
 Run("Day 8 part one", "Haunted Wasteland", PartOne);
+Run("Day 8 part two", "Haunted Wasteland", PartTwo);
